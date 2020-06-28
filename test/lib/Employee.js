42f2630fbd7a3210b49583be465f98e127f133bb
Mason <Mudland2721@gamil.com>
2020-06-28 16:35:02 -0600

    corrected Employee syn and added add emp function

---
+++ test/lib/Employee.js
@@ -0,0 +1,13 @@
+const Employee = require("./Employee");
+
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    };
+
+};
+
+module.exports = Employee;
+// TODO: Write code to define and export the Employee class
